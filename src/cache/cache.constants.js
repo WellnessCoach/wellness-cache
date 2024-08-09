c27fa899828a1ae13f60f05db3000b00cd1a1e23
Andrii Demchenko <andrey.demchenko@wellnesscoach.live>
2024-08-09 17:15:42 +0300

    Added subscription prefix

--- src/cache/cache.constants.js
+++ src/cache/cache.constants.js
@@ -5,6 +5,7 @@ const { TimeInMs } = require('../common/server/constants');
 module.exports = Object.freeze({
   CACHE_PREFIXES: Object.freeze({
     GET_COACH_AVAILABILITY_REPO: 'GET_HOME_COACH_AVAILABILITY_V1_REPO',
+    GET_USER_TYPE_SUBSCRIPTION: 'GET_USER_TYPE_SUBSCRIPTION',
   }),
   REDIS_ERROR_TIMEOUT: 1 * TimeInMs.Minute,
 });
